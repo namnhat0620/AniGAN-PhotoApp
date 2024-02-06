ca9f8814da08f6ebf2420c8fb2a863a857bb0677
namnhat0620 <94464940+namnhat0620@users.noreply.github.com>
2024-02-07 00:07:39 +0700

    Choose ani-style photo

--- app/src/main/java/com/kltn/anigan/ui/AIToolScreen.kt
+++ app/src/main/java/com/kltn/anigan/ui/AIToolScreen.kt
@@ -38,6 +38,7 @@ import com.kltn.anigan.R
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 import java.util.Objects
 
 @Composable
@@ -167,7 +168,7 @@ private fun InsertImage(modifier: Modifier = Modifier) {
 
 @Composable
 fun Context.createImageFile(): File {
-    val timeStamp = SimpleDateFormat("yyyy_MM_dd_HH:mm:ss").format(Date())
+    val timeStamp = SimpleDateFormat("yyyy_MM_dd_HH:mm:ss", Locale.getDefault()).format(Date())
     val imageFileName = "JPEG_" + timeStamp + "_"
     return File.createTempFile(
         imageFileName,
